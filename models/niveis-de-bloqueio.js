a0c48abaa6afc000958e69a7c0a415c9d41ddac8
RuglesChaves <40721659+RuglesChaves@users.noreply.github.com>
2018-08-25 13:24:54 -0400

    NOVO LAYOUT

--- models/niveis-de-bloqueio.js
+++ models/niveis-de-bloqueio.js
@@ -4,13 +4,19 @@ var mongoose = require('mongoose');
 var mongoosePaginate = require('mongoose-paginate');
 
 var schema = new mongoose.Schema({
+    'validar-por': String, //pode ser entrada ou saida
+    _usuario: { type: mongoose.Schema.Types.ObjectId, ref: 'usuario' },
+    'ultima-atualizacao': Date,
     nome: { type: String, uppercase: true },
-    nivel: String,
-    feriado: String,
-   // equipamento: [{ type: String, uppercase: true }],
-    semana: [{ type: String, uppercase: true }],
-    ip: [{ type: String, uppercase: true }]
-});
+    equipamento: String,
+    configs:[{
+        hora_inicio: String,
+        hora_fim: String,
+        sentido: String,
+        dias: [{ type: String }],
+       
+    }]
+}); 
 schema.plugin(mongoosePaginate);
 
 var Model = mongoose.model('niveis-de-bloqueio', schema);
